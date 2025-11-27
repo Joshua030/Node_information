3e9a7d1b94a5e3f80777c784fc16020b760df923
Jose Luis <joseluis.ceballos@techfriendly.es>
2025-11-28 00:48:18 +0100

    practice streams

--- streams/read-big/readBig.js
+++ streams/read-big/readBig.js
@@ -10,14 +10,47 @@ const fs = require("node:fs/promises");
   });
   const streamWrite = fileHandleWrite.createWriteStream();
 
+  let splitRemainder = "";
+
   streamRead.on("data", (chunk) => {
     const numbers = chunk.toString("utf-8").split("  ");
-    if (!streamWrite.write(chunk)) {
-      streamRead.pause();
+
+    if (Number(numbers[0]) !== Number(numbers[1]) - 1) {
+      if (splitRemainder) {
+        numbers[0] = splitRemainder.trim() + numbers[0].trim();
+        splitRemainder = "";
+      }
     }
+
+    if (
+      Number(numbers[numbers.length - 2]) + 1 !==
+      Number(numbers[numbers.length - 1])
+    ) {
+      console.error("Data corrupted");
+      splitRemainder = numbers.pop();
+    }
+
+    numbers.forEach((num) => {
+      let n = Number(num.trim());
+      if (n % 2 === 0) {
+        if (!streamWrite.write(" " + n + " ")) {
+          streamRead.pause();
+        }
+      }
+    });
   });
 
   streamWrite.on("drain", () => {
     streamRead.resume();
+  });
+
+  streamRead.on("end", async () => {
+    if (splitRemainder) {
+      console.error("Data corrupted at the end of the file");
+    }
+    console.log("Finished reading and writing.");
+    console.timeEnd("readBig");
+    await fileHandleRead.close();
+    await fileHandleWrite.close();
   });
 })();
